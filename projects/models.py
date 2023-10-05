198d261612b9572801cdb2e6ba00840b6308921f
Vasile Tanasa <vasiletanasa@gmail.com>
2023-10-05 19:17:59 +0300

    links

--- projects/models.py
+++ projects/models.py
@@ -5,3 +5,4 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FileField(upload_to="project_images/", blank=True)
+    link = models.URLField(max_length=200, default='')
